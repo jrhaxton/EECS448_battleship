948cd88545a4fca0ad4f89bb17efbae9fc3b303a
Josh Haxton <haxtonjr@gmail.com>
2020-09-10 20:08:25 -0500

    Added in routing to the interaction component as well as a skeleton for the playGame function and the win function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoardComponent } from './board/board.component';
+import { InteractionComponent } from './interaction/interaction.component';
 
 const routes: Routes = [
+  {path: 'game', component: InteractionComponent},
+  {path: '', redirectTo: 'game', pathMatch: 'full' },
   {path: 'board', component: BoardComponent}
 ];
 
--- src/app/interaction/interaction.component.ts
+++ src/app/interaction/interaction.component.ts
@@ -12,4 +12,57 @@ export class InteractionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+/*playGame() // Used to control the user's interaction as they play the game
+{
+  (if player == 1)
+  {
+    ...
+    shoot(); //function for player one to shoot at player two's board
+    ...
+    if(win(player two's board)==true)
+    {
+      Output the game is over and player one won
+      return();
+    }
+    else
+    {
+      player = 2;
+    }
+  }
+  else
+  {
+    ...
+    shoot();//function for player two to shoot at player one's board
+    ...
+    if(win(player one's board)==true)
+    {
+      Output the game is over and player two won
+      return();
+    }
+    else
+    {
+      player = 1;
+    }
+  }
+playGame();
+}
+
+
+win(board) //Determines whether or not a player has won the game
+{
+boolean gameOver = true
+  for(int i=1; i<10; i ++)
+  {
+    for(int j=1; j<10; j++)
+    {
+       if(board[i][j] == symbol/number for ship)
+       {
+         gameOver = false;
+         return(gameOver);
+       }
+    }
+  }
+return(gameOver);
+}
+*/
 }
