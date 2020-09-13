cad5ee5d9ec54b2f81178005e5ec35e7c48068be
Grant Keebler <g072k474@ku.ed>
2020-09-12 20:25:46 -0500

    added a skeleton for the hitOrMiss function

--- src/app/interaction/interaction.component.ts
+++ src/app/interaction/interaction.component.ts
@@ -12,4 +12,36 @@ export class InteractionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+}
+function hitOrMiss(row: number, col: number, player: number): boolean 
+{
+ if(player==1)
+ {
+   for(let i=0; i< 9; i++)
+   {
+     for(let j=0; i<9; j++)
+     {
+       if(i+1==row && j+1==col)
+       {
+         //Check to see if the ship is on the space 
+         return true;
+       }
+     }
+   }
+ }
+ else if(player==2)
+ {
+  for(let i=0; i< 9; i++)
+  {
+    for(let j=0; i<9; j++)
+    {
+      if(i+1==row && j+1==col)
+      {
+        //Check to see if the ship is on the space 
+        return true;
+      }
+    }
+  }
+ }
 }
