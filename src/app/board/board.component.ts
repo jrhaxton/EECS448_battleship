57f8ac853625ad81a7139d03098e21ec597c2960
Josh Haxton <haxtonjr@gmail.com>
2020-09-13 12:45:36 -0500

    Added in error handling and expanded comments

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -19,9 +19,10 @@ export class BoardComponent implements OnInit {
   gotShips: boolean; // Used to know if the number of ships has been given
   coords: string; // Used to convert the user's ship coordinates into useable values
   col: number; // Used to select column to place ship
-  row: number; // Used to select row to place ship
+  row: number = 1; // Used to select row to place ship
   dir: string; // Used to know if the ship is horizontal or vertical
-
+  validCoords: boolean; // Used to verify that a coordinate is valid
+  validNum: boolean; // Used to verify if the number of ship is valid
   constructor() { }
 
   attack1(row,col){
@@ -46,190 +47,248 @@ export class BoardComponent implements OnInit {
     }
   }
 
-playGame() // Used to control the user's interaction as they play the game
-{
-  if(this.player == 1)
+  playGame() // Used to control the user's interaction as they play the game
   {
-    //...
-    //shoot(); //function for player one to shoot at player two's board
-    //...
-    if(this.win(this.board2)==true)
-    {
-      //Output the game is over and player one won
-      return;
-    }
-    else
-    {
-      this.player = 2;
-    }
-  }
-  else
-  {
-    //...
-    //shoot();//function for player two to shoot at player one's board
-    //...
-    if(this.win(this.board1)==true)
-    {
-      //Output the game is over and player two won
-      return;
-    }
-    else
-    {
-      this.player = 1;
-    }
-  }
-this.playGame();
-}
-
-win(board) //Determines whether or not a player has won the game
-{
-  this.gameOver = true
-  for(let i=1; i<10; i ++)
-  {
-    for(let j=1; j<10; j++)
-    {
-       if(board[i][j] == 's')
-       {
-         this.gameOver = false;
-         return(this.gameOver);
-       }
-    }
-  }
-return(this.gameOver);
-}
-
-//Function used to take in the number of ships and helps keep track of the length/number of ships that players need to place
-numShips(event:any)
-{
- this.ships = event.target.value;
- this.p1Ships = this.ships;
- this.p2Ships = this.ships
-}
-
-//Function used to let the program know that the number of ships has been entered whent the button is clicked
-enterShips()
-{
- this.gotShips = true;
-}
-
-//Function used to take in the user's placement for their ships and convert them into useable values
-getCoords(event:any)
-{
-  this.coords = event.target.value.split(",", 3);
-  this.col = this.convertLetter(this.coords[0]);
-  this.row = Number(this.coords[1]);
-  this.dir = this.coords[2];
-}
-
-//Function used to place the ships on the board
-placeShips(col, row, direction:string, board, length:number)
-{
-  if (this.checkPlacements(row, col, direction, board, length)) 
-  {
-    for (let i = 0; i < length; i++) 
-    {
-      if (direction == 'H' || direction == 'h') 
-      {
-        board[row][col + i] = 's';
-      } 
-      else 
-      {
-        board[row + i][col] = 's';
-      }
-    }
     if(this.player == 1)
     {
-      this.p1Ships--;
-      if(this.p1Ships == 0)
+      //...
+      //shoot(); //function for player one to shoot at player two's board
+      //...
+      if(this.win(this.board2)==true)
+      {
+        //Output the game is over and player one won
+        return;
+      }
+      else
       {
         this.player = 2;
       }
     }
     else
     {
-      this.p2Ships--;
-      if(this.p2Ships == 0)
+      //...
+      //shoot();//function for player two to shoot at player one's board
+      //...
+      if(this.win(this.board1)==true)
+      {
+        //Output the game is over and player two won
+        return;
+      }
+      else
       {
         this.player = 1;
       }
     }
-  this.updateBoards(); 
+  this.playGame();
   }
-}
 
-// Function used to verify if a ship can be placed given the coordinates
-checkPlacements(row, col:number, dir:string, board, length:number)
-{
-  let valid = true;
-  for (let i = 0; i < length; i++) {
-    if (dir.toUpperCase() == 'H') {
-      if(board[row][col + i] != '~')
+  win(board) //Determines whether or not a player has won the game
+  {
+    this.gameOver = true
+    for(let i=1; i<10; i ++)
+    {
+      for(let j=1; j<10; j++)
       {
-        valid = false;
-      }
-    } 
-    else {
-      if(board[row + i][col] != '~')
-      {
-        valid = false;
+        if(board[i][j] == 's')
+        {
+          this.gameOver = false;
+          return(this.gameOver);
+        }
       }
     }
+  return(this.gameOver);
   }
-  return(valid);
-}
 
-// Function used to convert the column letter to a numerical value
-convertLetter(letter:string)
-{
-  let num = 0;
-  if( letter.toUpperCase() == 'A')
+  /*
+  - Function used to take in the number of ships and helps keep track of the length/number of ships that players need to place
+  - @param {any} event The input string from the user for the number of ships
+  */
+  numShips(event:any)
   {
-    num = 1;
+    if(Number(event.target.value) > 0 && Number(event.target.value) < 6)
+    {
+      this.ships = event.target.value;
+      this.p1Ships = this.ships;
+      this.p2Ships = this.ships
+      this.validNum = true
+    }
+    else{
+      this.validNum = false;
+    }
   }
-  else if( letter.toUpperCase() == 'B')
-  {
-    num = 2;
-  }
-  else if( letter.toUpperCase() == 'C')
-  {
-    num = 3;
-  }
-  else if( letter.toUpperCase() == 'D')
-  {
-    num = 4;
-  }
-  else if( letter.toUpperCase() == 'E')
-  {
-    num = 5;
-  }
-  else if( letter.toUpperCase() == 'F')
-  {
-    num = 6;
-  }
-  else if( letter.toUpperCase() == 'G')
-  {
-    num = 7;
-  }
-  else if( letter.toUpperCase() == 'H')
-  {
-    num = 8;
-  }
-  else if( letter.toUpperCase() == 'I')
-  {
-    num = 9;
-  }
-  else{
-    //error
-  }
-  return (num);
-}
 
-// Function used to update the boards after ship placement
-updateBoards()
-{
-  this.board1= [...this.board1];
-  this.board2= [...this.board2];
-}
+  /*
+  - Function used to let the program know that the number of ships has been entered whent the button is clicked
+  */
+  enterShips()
+  {
+    if(!this.validNum)
+    {
+      alert("Not a valid number");
+    }
+    else{
+      this.gotShips = true
+    }
+  }
+
+  /*
+  - Function used to take in the user's placement for their ships and convert them into useable values
+  - @param {any} event The input string from the user for the coordinates of the ships head and orientation
+  */
+  getCoords(event:any)
+  {
+    this.validCoords = false;
+    this.coords = event.target.value.split(",", 3);
+    if(this.convertLetter(this.coords[0]) != 0 && (Number(this.coords[1]) > 0 && Number(this.coords[1]) < 10) && (this.coords[2].toUpperCase() == 'H' || this.coords[2].toUpperCase() == 'V'))
+    {
+      this.col = this.convertLetter(this.coords[0]);
+      this.row = Number(this.coords[1]);
+      this.dir = this.coords[2];
+      this.validCoords = true
+    }
+  }
+
+  /*
+  - Function used to place the ships on the board
+  - @param {number} col The column number for the head of the ship
+  - @param {number} row The row number for the head of the ship
+  - @param {string} direction The string/char symbolzing horizontal or vertical alignment
+  - @param {any} board The board being marked with ships
+  - @param {number} length The length/size of the ship being placed
+  */
+  placeShips(col: number, row: number, direction:string, board: any, length:number)
+  {
+    if(this.validCoords == true)
+    {
+      if (this.checkPlacements(row, col, direction, board, length)) 
+      {
+        for (let i = 0; i < length; i++) 
+        {
+          if (direction == 'H' || direction == 'h') 
+          {
+            board[row][col + i] = 's';
+          } 
+          else 
+          {
+            board[row + i][col] = 's';
+          }
+        }
+        if(this.player == 1)
+        {
+          this.p1Ships--;
+          if(this.p1Ships == 0)
+          {
+            this.player = 2;
+          }
+        }
+        else
+        {
+          this.p2Ships--;
+          if(this.p2Ships == 0)
+          {
+            this.player = 1;
+          }
+        }
+      this.updateBoards(); 
+      }
+    }
+    else{
+      alert("Invalid coordinates");
+    }
+  }
+
+  /* 
+  - Function used to verify if a ship can be placed given the coordinates
+  - @param {number} row The row number for the head of the ship
+  - @param {number} col The column number for the head of the ship
+  - @param {string} dir The string/char symbolzing horizontal or vertical alignment
+  - @param {any} board The board being marked with ships
+  - @param {number} length The length/size of the ship being placed
+  - @return {boolean} If placement of the ship is valid
+  */
+  checkPlacements(row:number, col:number, dir:string, board, length:number)
+  {
+    let valid = true;
+    for (let i = 0; i < length; i++) {
+      if (dir.toUpperCase() == 'H') {
+        if(board[row][col + i] != '~')
+        {
+          valid = false;
+        }
+      } 
+      else {
+        if(board[row + i][col] != '~')
+        {
+          valid = false;
+        }
+      }
+    }
+    return(valid);
+  }
+
+  /*
+  - Function used to convert the column letter to a numerical value
+  - @param {string} letter The letter corresponding to a col of the board
+  - @return {number} A numeric value corresponding to valid letters
+  */
+  convertLetter(letter:string)
+  {
+    let num = 0;
+    if( letter.toUpperCase() == 'A')
+    {
+      num = 1;
+    }
+    else if( letter.toUpperCase() == 'B')
+    {
+      num = 2;
+    }
+    else if( letter.toUpperCase() == 'C')
+    {
+      num = 3;
+    }
+    else if( letter.toUpperCase() == 'D')
+    {
+      num = 4;
+    }
+    else if( letter.toUpperCase() == 'E')
+    {
+      num = 5;
+    }
+    else if( letter.toUpperCase() == 'F')
+    {
+      num = 6;
+    }
+    else if( letter.toUpperCase() == 'G')
+    {
+      num = 7;
+    }
+    else if( letter.toUpperCase() == 'H')
+    {
+      num = 8;
+    }
+    else if( letter.toUpperCase() == 'I')
+    {
+      num = 9;
+    }
+    else if( letter.toUpperCase() == '')
+    {
+      num = 1;
+    }
+    else{
+      num = 0;
+    }
+    return (num);
+  }
+
+  /* 
+  - Function used to update the boards after ship placement
+  */
+  updateBoards()
+  {
+    this.board1= [...this.board1];
+    this.board2= [...this.board2];
+  }
+
   ngOnInit(): void {
     this.player= 1;
     this.gotShips = false;
